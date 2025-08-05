137d79d8842bee0da323e5e994e969520934cca2
zhangyafeng <zhangyafeng@range-soft.com>
2025-08-05 16:12:54 +0800

    <feat> 常用工具类搭建

--- src/main/java/com/xyy/codeai/CodeAiApplication.java
+++ src/main/java/com/xyy/codeai/CodeAiApplication.java
@@ -2,8 +2,10 @@ package com.xyy.codeai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class CodeAiApplication {
 
     public static void main(String[] args) {
