84581194b35a51799ee220f4ce6567b54ebc2d13
zhangyafeng <zhangyafeng@range-soft.com>
2025-08-05 14:33:44 +0800

    <build> 初始化项目

---
+++ src/main/java/com/xyy/codeai/CodeAiApplication.java
@@ -0,0 +1,13 @@
+package com.xyy.codeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAiApplication.class, args);
+    }
+
+}
