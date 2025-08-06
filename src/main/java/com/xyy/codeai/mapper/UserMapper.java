7538491b55adafac1369f28e66c6ec3e7a0292d7
zhangyafeng <zhangyafeng@range-soft.com>
2025-08-06 16:22:34 +0800

    <feat> 未提交文件

---
+++ src/main/java/com/xyy/codeai/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xyy.codeai.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xyy.codeai.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author java_boy
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
