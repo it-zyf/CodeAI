4216633a9f618a5cf66590fd51f1eb9772b3cb7d
zhangyafeng <zhangyafeng@range-soft.com>
2025-08-13 10:53:23 +0800

    <feat> 流式返回

--- src/main/java/com/xyy/codeai/service/AiCodeGeneratorService.java
+++ src/main/java/com/xyy/codeai/service/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.xyy.codeai.service;
 import com.xyy.codeai.model.HtmlCodeResult;
 import com.xyy.codeai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
 
@@ -23,4 +24,27 @@ public interface AiCodeGeneratorService {
      */
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
     MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI 的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI 的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
+
 }
--- src/test/java/com/xyy/codeai/core/AiCodeGeneratorFacadeTest.java
+++ src/test/java/com/xyy/codeai/core/AiCodeGeneratorFacadeTest.java
@@ -2,10 +2,13 @@ package com.xyy.codeai.core;
 
 import com.xyy.codeai.model.enums.CodeGenTypeEnum;
 import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * @author zyf
@@ -22,4 +25,16 @@ class AiCodeGeneratorFacadeTest {
         File file = aiCodeGeneratorFacade.generateAndSaveCode("生成一个登录页面", CodeGenTypeEnum.HTML);
         System.out.println(file.getAbsolutePath());
     }
+
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+
 }
