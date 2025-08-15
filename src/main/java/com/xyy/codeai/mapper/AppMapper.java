bc8a5a355522823e27fc0a804146147fa49ec699
zhangyafeng <zhangyafeng@range-soft.com>
2025-08-15 17:38:38 +0800

    <fix> 代码调整

---
+++ src/main/java/com/xyy/codeai/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.xyy.codeai.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xyy.codeai.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author java_boy
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
